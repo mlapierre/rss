3708d3f995992d65afac3ca4ad287b471642aee6
Mark Lapierre <mlapierre@gmail.com>
2015-05-06 09:43:44 -0400

    Enable hotkeys 'n' & 'p' for navigating through articles

--- reader/app/controllers/controllers.js
+++ reader/app/controllers/controllers.js
@@ -6,10 +6,48 @@ angular.module('readerAppControllers', [])
 }])
 
 .controller('mainCtrl', function ($scope, $document) {
+  var keyHanders = {
+    'n': handleNextArticle,
+    'p': handlePrevArticle
+  };
+
+  function handleNextArticle() {
+    var scope = angular.element($('#entries_view')).scope();
+    if (scope.selectedIndex !== scope.$$childTail.$index) {
+      scope.selectedIndex++;
+      scope.$apply();
+    }
+  }
+
+  function handlePrevArticle() {
+    var scope = angular.element($('#entries_view')).scope();
+    if (scope.selectedIndex - 1 < 0) {
+      scope.selectedIndex = 0;
+    } else {
+      scope.selectedIndex--;
+    }
+    scope.$apply();
+  }
+
+  function processKeypress(key) {
+    if (typeof keyHanders[key] === 'function') {
+      return keyHanders[key](key);
+    }
+  }
+
+  function getChar(event) {
+    if (event.which === null) {
+      return String.fromCharCode(event.keyCode) // IE
+    } else if (event.which !== 0 && event.charCode !== 0) {
+      return String.fromCharCode(event.which)   // the rest
+    } else {
+      return null // special key
+    }
+  }
+
   function keypressHandler(keyEvent) {
     console.log('keypress', keyEvent);
-
-    //$scope.$apply(); // remove this line if not need
+    processKeypress(getChar(keyEvent));
   }
 
   $document.on('keypress', keypressHandler);
